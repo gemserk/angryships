e334532a0e8dfe54967f08fed512f3626bfd428b
Ariel Coppes <ariel.coppes@gemserk.com>
2011-10-15 15:24:52 -0200

    changed the PixmapWorld to implement disposable

--- angryships-core/src/main/java/com/gemserk/games/angryships/components/PixmapWorld.java
+++ angryships-core/src/main/java/com/gemserk/games/angryships/components/PixmapWorld.java
@@ -1,9 +1,10 @@
 package com.gemserk.games.angryships.components;
 
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
 import com.gemserk.prototypes.pixmap.PixmapHelper;
 
-public class PixmapWorld {
+public class PixmapWorld implements Disposable {
 	
 	Array<PixmapHelper> pixmaps = new Array<PixmapHelper>();
 
@@ -17,6 +18,13 @@ public class PixmapWorld {
 	
 	public Array<PixmapHelper> getPixmaps() {
 		return pixmaps;
+	}
+
+	@Override
+	public void dispose() {
+		for (int i = 0; i < pixmaps.size; i++) 
+			pixmaps.get(i).dispose();
+		pixmaps.clear();
 	}
 	
 }
